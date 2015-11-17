5893275b675ff9fb0a31f82c8da580aeef426bec
Noah Shillington <nshillin@ualberta.ca>
2015-11-17 13:37:58 -0700

    Parse Review turns Reviews into Dictionary

--- Project/Phase3.py
+++ Project/Phase3.py
@@ -1,9 +1,11 @@
-from bsddb3 import db
+#from bsddb3 import db
+from csv import reader
 
 reviewsColumns = ["productId","title","price","userId","profileName","helpfulness","score","time","summary","text"]
+#infile = ['A,B,C,"D12121",E,F,G,H,"I9,I8",J,K']
 
-database = db.DB()
-main()
+#print reader(infile).next()
+#database = db.DB()
 
 def main():
     while True:
@@ -11,11 +13,13 @@ def main():
         if text == "exit":
             return
         else:
-            queryParser(text)
+            parseReview(0)
+        #    parseQuery(text)
 
-def queryParser(text):
+
+def parseQuery(text):
     queryList = text.split()
-    
+
     validQuery = False
 
     if validQuery:
@@ -23,8 +27,16 @@ def queryParser(text):
     else:
         print('"'+text+'" is not a valid query')
 
-def reviewParser(review):
-    parsedReview = {}
-    for c in reviewsColumns:
-        reviewSegment = ""
-        parsedReview[c] = reviewSegment
+def executeQuery():
+    pass
+
+def parseReview(reviewNumber):
+    ####database = db.DB()
+    #database.open("rw.idx")
+    #review = database.get(reviewNumber).decode("utf-8")
+    #reviewItems = reader(review).next()
+    reviewItems = ["1","2","3","4","5","6","7","8","9","10"]
+    parsedReview = dict(zip(reviewsColumns, reviewItems))
+    print parsedReview
+
+main()
