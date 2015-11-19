7769d6eb5fec765a392ff8aac090acee9ecd6655
Noah Shillington <nshillin@ualberta.ca>
2015-11-19 13:34:18 -0700

    Moved previous commit stuff to method

--- Project/Phase3.py
+++ Project/Phase3.py
@@ -16,12 +16,24 @@ def main():
 
 def parseQuery(text):
     queryList = text.split()
+    organizedQueryList = organizeQueries(queryList)
+    if len(organizedQueryList) == 0:
+        return
+    for query in organizedQueryList:
+        if ('<' in query) or ('>' in query):
+            compareQuery(query)
+        # Some other query type
+
+    printReviews()
+
+
+#### Fixes < and > queries, as well as adds them to the end of the organizedQueryList
+# ,this is used to increase efficiency, as < and > require going through all of
+# the reviews, whereas other commands can just find the key and immediately find
+# the result
+def organizeQueries(queryList):
     organizedQueryList = []
     i = 0
-    # Fixes < and > queries, as well as adds them to the end of the organizedQueryList
-    # ,this is used to increase efficiency, as < and > require going through all of
-    # the reviews, whereas other commands can just find the key and immediately find
-    # the result
     while i < len(queryList):
         query = queryList[i]
         if (i+2 < len(queryList) and (queryList[i+1] == '<' or queryList[i+1] == '>')):
@@ -30,35 +42,30 @@ def parseQuery(text):
         elif (i+1 < len(queryList) and ('<' in queryList[i+1] or '>' in queryList[i+1] or '<' in queryList[i] or '>' in queryList[i])):
             query = queryList[i] + queryList[i+1]
             i+=1
-
+            
         # Something should be here to stop invalid queries
         if false:
             print('"'+text+'" is not a valid query')
-            return
+            return []
+
         if ('<' in query) or ('>' in query):
             organizedQueryList.append(query)
         else:
             organizedQueryList.insert(0,query)
-        print query
         i+=1
+    return organizedQueryList
 
-    for query in organizedQueryList:
-        if ('<' in query) or ('>' in query):
-            compareQuery(query)
-
-    printReviews()
-        
 
 ## Start of query handlers
 # Query handlers should accept a command and
-# Return a list of review keys
 
+# Return a list of review keys
 def compareQuery(query):
     reviewList = []
     return reviewList
 
 
-## End of query handlers
+# End of query handlers
 
 # Turns Reviews into Dictionary
 def parseReview(reviewNumber):
